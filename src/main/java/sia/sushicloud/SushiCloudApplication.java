552431f3efe5ea73e1d52554b0ae9573fda18198
kamilK <kamil.kukowski@gmail.com>
2021-12-12 21:07:17 +0100

    FEAT: Initial commit

---
+++ src/main/java/sia/sushicloud/SushiCloudApplication.java
@@ -0,0 +1,13 @@
+package sia.sushicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiCloudApplication.class, args);
+	}
+
+}
